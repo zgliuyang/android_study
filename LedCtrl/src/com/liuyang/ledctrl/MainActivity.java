ae49e56641a3682bb2343f898f5e5335662260de
zgliuyang <354587339@qq.com>
2020-05-21 00:08:48 +0800

    v1 create

---
+++ LedCtrl/src/com/liuyang/ledctrl/MainActivity.java
@@ -0,0 +1,61 @@
+package com.liuyang.ledctrl;
+
+import android.R.bool;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+public class MainActivity extends Activity {
+	
+	private Button button = null;
+	private CheckBox checkBoxLed1 = null;
+	private CheckBox checkBoxLed2 = null;
+	private CheckBox checkBoxLed3 = null;
+	private CheckBox checkBoxLed4 = null;
+	private boolean ledon = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        button = (Button)findViewById(R.id.BUTTON);
+        checkBoxLed1 = (CheckBox)findViewById(R.id.LED1);
+        checkBoxLed2 = (CheckBox)findViewById(R.id.LED2);
+        checkBoxLed3 = (CheckBox)findViewById(R.id.LED3);
+        checkBoxLed4 = (CheckBox)findViewById(R.id.LED4);
+        button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				ledon = !ledon;
+				if(ledon)
+				{
+					button.setText("LED ALL OFF");
+					checkBoxLed1.setChecked(ledon);
+					checkBoxLed2.setChecked(ledon);
+					checkBoxLed3.setChecked(ledon);
+					checkBoxLed4.setChecked(ledon);
+				}
+				else
+					button.setText("LED ALL ON");
+					checkBoxLed1.setChecked(ledon);
+					checkBoxLed2.setChecked(ledon);
+					checkBoxLed3.setChecked(ledon);
+					checkBoxLed4.setChecked(ledon);
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
